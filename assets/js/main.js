487436bbe9a1440d666d1bacc9da0991f481e818
Jack <60094923+JackyMeslin@users.noreply.github.com>
2022-12-01 12:12:53 +0100

    page accueil - background vidéo

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,3 +260,7 @@
 			});
 
 })(jQuery);
+
+/* Note Codepen is importing the jQuery Background Video plugin - https://github.com/BGStock/jquery-background-video */
+
+$('.jquery-background-video').bgVideo({fadeIn: 2000});
